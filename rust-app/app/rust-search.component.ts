004a961fa4165bf4d916e4d88f2f63074c404ecf
AlexSoto <alexander.soto@rutgers.edu>
2017-02-14 01:34:25 +0000

    Added Observable data source for autocomplete search

--- rust-app/app/rust-search.component.ts
+++ rust-app/app/rust-search.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { FormControl } from '@angular/forms';
 import { SchoolDataService } from './school-data.service';
 
@@ -10,23 +13,96 @@ import { SchoolDataService } from './school-data.service';
 })
 export class RustSearchComponent { 
   
-  mySource: string[] = ['alpha', 'beta', 'charlie', 'delta'];
+  autoCompleteSource: any[]; // = []; // = []; // = ['alpha', 'beta', 'charlie', 'delta'];
   
+  /*
+   * Search options: School, Subject, Course, Professor, CoreCode
+   */
+  twoNumRegex = /[0-9]{2}/g;
+  threeNumRegex = /[0-9]{3}/g;
+  stringRegex = /([a-z]|\s)+/ig;
+  // twoNumMsg = ["schools"];
+  // threeNumMsg = ["subjects","courses"];
+  // strMsg = ["course titles", "instructors", "core codes"];
+  
+  // regexArr = [
+  //   {"regex": /[0-9]{2}/g, "messages":["schools"]},
+  //   {"regex": /[0-9]{3}/g, "messages":["subjects","courses"]},
+  //   {"regex": /([a-z]|\s)+/ig, "messages":["course titles", "instructors", "core codes"]},
+  // ];
+  regexArr = [
+    {
+      "regex": this.twoNumRegex, 
+      "options":[{
+        id:"schools",
+        text:"school codes"
+        }]
+    },
+    {
+      "regex": this.threeNumRegex, 
+      "options":[{
+        id:"subjects", 
+        text:"subject codes"
+        },{
+        id:"courseNum",
+        text:"course codes"
+        }]
+    },
+    {
+      "regex": this.stringRegex, 
+      "options":[{
+        id:"courseTitle",
+        text:"course titles"
+      }, {
+        id:"instructors",
+        text:"instructors"
+      },{
+        id:"coreCodes",
+        text:"core codes"
+      }]
+    }
+  ];
+  
+  findRegexMessages() {
+    // let messages: string[] = [];
+    this.autoCompleteSource = [];
+    for (let i in this.regexArr){
+      // console.log(`this.regexArr[${i}]["regex"].test(${val})=${this.regexArr[i]["regex"].test(val)}`);
+      if (this.regexArr[i]["regex"].test(this.searchParams[this.searchParams.length-1])){
+        // messages = this.regexArr[i]["messages"];
+        for (let j in this.regexArr[i]["options"]){
+          this.autoCompleteSource.push(this.regexArr[i]["options"][j]);
+        }
+        
+      }
+    }
+    // this.autoCompleteSource = messages;
+    // return messages;
+  }
   searchParams: string[] = [];
   search = new FormControl();
+  observableSource(){
+    return Observable.of(this.autoCompleteSource);
+  };
   
-  constructor(){
-    this.logSearchChange();
+  autocompleListFormatter = (data: any) : SafeHtml => {
+    let html = `<span>Find '<i>${this.searchParams}</i>' in ${data.text}</span>`;
+    return this._sanitizer.bypassSecurityTrustHtml(html);
   }
   
-  logSearchChange(){
-    this.search.valueChanges.forEach(val => {
+  testVar = search;
+  
+  constructor(private _sanitizer: DomSanitizer){
+    this.autoCompleteSource = [];
+    
+    this.search.valueChanges
+    .distinctUntilChanged()
+    .subscribe(val => {
+      this.searchParams = [];
       this.searchParams.push(val);
-      
-      // if (this.searchParams.length >= 2){
-        
-      // }
-    })
+      this.findRegexMessages();
+    });
+    
+    
   }
-  
 }

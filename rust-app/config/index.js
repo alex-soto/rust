7ecb4d8dbeb827ec557f0f5a11661531fea0f7cc
AlexSoto <alexander.soto@rutgers.edu>
2017-03-24 20:58:33 +0000

    Updated config module

--- rust-app/config/index.js
+++ rust-app/config/index.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-    	"dbURI": "mongodb://rudatauser:Zp13409603761hTKk23498101349asov23478095hwKZLAJSUEjwqm92134@ds139899.mlab.com:39899/rudata"
+        dbURI: process.env.dbURI
     };
 } else {
     module.exports = require('./development.json');

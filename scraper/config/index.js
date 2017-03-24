a048c98d87ecba55c5dea3e1485db107bef9331a
AlexSoto <alexander.soto@rutgers.edu>
2017-03-24 16:16:31 +0000

    Updated config/index.js file

--- scraper/config/index.js
+++ scraper/config/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./production.json');
+    // module.exports = require('./production.json');
+    module.exports = {
+        "dbURI": process.env.dbURI
+    };
+    
 } else {
     module.exports = require('./development.json');
 }
